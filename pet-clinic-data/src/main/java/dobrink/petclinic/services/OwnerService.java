c045e6162c5fa1df2fab2f4e1b2a6f2dbe0d0088
DobrinKazandziev <dobrinkazandziev94@gmail.com>
2019-06-30 19:39:53 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/dobrink/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package dobrink.petclinic.services;
+
+import dobrink.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+  Owner findByLastName(String lastName);
+
+  Owner findById(Long id);
+
+  Owner save(Owner owner);
+
+  Set<Owner> findAll();
+}
