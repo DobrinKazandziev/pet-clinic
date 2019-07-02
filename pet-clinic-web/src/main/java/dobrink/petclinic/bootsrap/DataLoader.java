f8059a8c460d3868c1c2fbc1578015dcf89c1c61
DobrinKazandziev <dobrinkazandziev94@gmail.com>
2019-07-02 18:02:57 +0200

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/dobrink/petclinic/bootsrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dobrink/petclinic/bootsrap/DataLoader.java
@@ -39,7 +39,6 @@ public class DataLoader implements CommandLineRunner {
   private Vet createFakeVet(Long id) {
     Vet vet = new Vet();
 
-    vet.setId(id);
     vet.setFirstName(faker.name().firstName());
     vet.setLastName(faker.name().lastName());
 
@@ -49,7 +48,6 @@ public class DataLoader implements CommandLineRunner {
   private Owner createFakeOwner(Long id) {
     Owner owner = new Owner();
 
-    owner.setId(id);
     owner.setFirstName(faker.name().firstName());
     owner.setLastName(faker.name().lastName());
 
@@ -60,14 +58,12 @@ public class DataLoader implements CommandLineRunner {
 //  public void run(String... args) throws Exception {
 //
 //    Owner owner1 = new Owner();
-//    owner1.setId(1L);
 //    owner1.setFirstName("Michael");
 //    owner1.setLastName("Weston");
 //
 //    ownerService.save(owner1);
 //
 //    Owner owner2 = new Owner();
-//    owner2.setId(2L);
 //    owner2.setFirstName("Fiona");
 //    owner2.setLastName("Glenanne");
 //
@@ -76,14 +72,12 @@ public class DataLoader implements CommandLineRunner {
 //    System.out.println("Loaded Owners...");
 //
 //    Vet vet1 = new Vet();
-//    vet1.setId(1L);
 //    vet1.setFirstName("Sam");
 //    vet1.setLastName("Axe");
 //
 //    vetService.save(vet1);
 //
 //    Vet vet2 = new Vet();
-//    vet2.setId(2L);
 //    vet2.setFirstName("Jessie");
 //    vet2.setLastName("Porter");
 //
